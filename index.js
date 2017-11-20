28f5bb66cad77249d0886f8b1842ea50241f2606
jakaprima <jakaprima123@gmail.com>
2017-11-20 09:36:01 +0700

    modify

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-/**
- * Escape special characters in the given string of html.
- *
- * @param  {String} html
- * @return {String}
- */
 module.exports = {
   escape: function(html) {
-    return String(html)
-      .replace(/&/g, '&amp;')
-      .replace(/"/g, '&quot;')
-      .replace(/'/g, '&#39;')
-      .replace(/</g, '&lt;')
-      .replace(/>/g, '&gt;');
+  return String(html)
+    .replace(/&/g, '&amp;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;');
   },
 
-  /**
-   * Unescape special characters in the given string of html.
-   *
-   * @param  {String} html
-   * @return {String}
-   */
+
   unescape: function(html) {
-    return String(html)
-      .replace(/&amp;/g, '&')
-      .replace(/&quot;/g, '"')
-      .replace(/&lt;/g, '<')
-      .replace(/&gt;/g, '>');
-  }
+  return String(html)
+    .replace(/&amp;/g, '&')
+    .replace(/&quot;/g, '"')
+    .replace(/&lt;/g, '<')
+    .replace(/&gt;/g, '>');
+  },
+
+  validate: {
+    number: function(val){
+      return /^[0-9]*$|^$|undefined/.test(val)
+    },
+    text: function(val){
+      return /^[a-zA-Z_ ]*$|^$|undefined/.test(val)
+    } 
+  }   
+
 };
